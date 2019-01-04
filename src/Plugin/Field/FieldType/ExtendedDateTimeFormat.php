581cea0b1abb7317adef6b14779da5b8b1d044aa
Seth Shaw <seth.shaw@unlv.edu>
2019-01-04 12:53:26 -0800

    support multiple EDTF data types in JSON-LD
    - update normalized array hook to update data type for edtf fields
    - update default configurations to use edtf field

---
+++ src/Plugin/Field/FieldType/ExtendedDateTimeFormat.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\controlled_access_terms\Plugin\Field\FieldType;
+
+use Drupal\Core\Field\Plugin\Field\FieldType\StringItem;
+
+/**
+ * Implements a Extended DateTime Format field.
+ *
+ * @FieldType(
+ *   id = "edtf",
+ *   label = @Translation("EDTF, level 1"),
+ *   module = "controlled_access_terms",
+ *   description = @Translation("Extended Date Time Format field"),
+ *   default_formatter = "edtf_default",
+ *   default_widget = "edtf_default",
+ * )
+ */
+class ExtendedDateTimeFormat extends StringItem {
+
+}
