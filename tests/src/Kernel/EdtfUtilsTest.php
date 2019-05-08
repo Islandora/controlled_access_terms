d420d68758eb3b6dc9e305a4ffe4d2eb9dd6f1cf
Jared Whiklo <jwhiklo@gmail.com>
2019-05-08 10:41:45 -0500

    Coder

--- tests/src/Kernel/EdtfUtilsTest.php
+++ tests/src/Kernel/EdtfUtilsTest.php
@@ -6,7 +6,8 @@ use Drupal\controlled_access_terms\EDTFUtils;
 use Drupal\KernelTests\KernelTestBase;
 
 /**
- * Class EdtfUtilsTest
+ * Class EdtfUtilsTest.
+ *
  * @package Drupal\Tests\controlled_access_terms\Kernel
  * @group controlled_access_terms
  * @coversDefaultClass \Drupal\controlled_access_terms\EDTFUtils
@@ -18,7 +19,7 @@ class EdtfUtilsTest extends KernelTestBase {
    *
    * @var array
    */
-  private $test_cases = [
+  private $testCases = [
     '1900' => [],
     '1900-01' => [],
     '1900-01-02' => [],
@@ -26,8 +27,10 @@ class EdtfUtilsTest extends KernelTestBase {
     '1900-XX' => [],
     '1900-91' => ['Provided month value \'91\' is not valid.'],
     '1900-91-01' => ['Provided month value \'91\' is not valid.'],
-    '1900-3X' => [], // no validation for months with X.
-    '1900-31' => [], // month 31 matches summer so its valid.
+    // No validation for months with X.
+    '1900-3X' => [],
+    // Month 31 without a day matches summer so it's valid.
+    '1900-31' => [],
     '1900-31-01' => ['Provided month value \'31\' is not valid.'],
     '190X-5X-8X' => [],
     '19000' => ['Years longer than 4 digits must be prefixed with a \'Y\'.'],
@@ -36,7 +39,8 @@ class EdtfUtilsTest extends KernelTestBase {
     '190' => ['Years must be at least 4 characters long.'],
     '190-99-52' => ['Years must be at least 4 characters long.',
       'Provided month value \'99\' is not valid.',
-      'Provided day value \'52\' is not valid.'],
+      'Provided day value \'52\' is not valid.'
+    ],
     '1900-01-02T' => ['Time not provided with time seperator (T).'],
     '1900-01-02T1:1:1' => ['The date/time \'1900-01-02T1:1:1\' is invalid.'],
     '1900-01-02T01:22:33' => [],
@@ -49,7 +53,7 @@ class EdtfUtilsTest extends KernelTestBase {
    * @covers ::validate
    */
   public function testEdtfValidate() {
-    foreach ($this->test_cases as $input => $expected) {
+    foreach ($this->testCases as $input => $expected) {
       $this->assertEquals($expected, EDTFUtils::validate($input, FALSE, FALSE, FALSE));
     }
   }
