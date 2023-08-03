4ce8ce97548315e9c35e572c59a2e07104be8c9e
Seth Shaw <sshaw28@asu.edu>
2023-08-03 10:52:53 -0700

    Revert "add/fix tests; coding style"; should have been a PR.
    This reverts commit a733909c894b00b19bf097b74f3b1268e67ae880.

--- tests/src/Kernel/EdtfUtilsTest.php
+++ tests/src/Kernel/EdtfUtilsTest.php
@@ -19,7 +19,7 @@ class EdtfUtilsTest extends KernelTestBase {
    *
    * @var array
    */
-  private $singleDateValidations = [
+  private $testCases = [
     '1900' => [],
     '1900-01' => [],
     '1900-01-02' => [],
@@ -28,16 +28,16 @@ class EdtfUtilsTest extends KernelTestBase {
     '1900-91' => ['Provided month value \'91\' is not valid.'],
     '1900-91-01' => ['Provided month value \'91\' is not valid.'],
     // No validation for months with X.
-    '1900-3X' => ['Provided month value \'3X\' is not valid.'],
+    '1900-3X' => [],
     // Month 31 without a day matches summer so it's valid.
     '1900-31' => [],
     '1900-31-01' => ['Provided month value \'31\' is not valid.'],
-    '190X-5X-8X' => ['Provided month value \'5X\' is not valid.'],
+    '190X-5X-8X' => [],
     '19000' => ['Years longer than 4 digits must be prefixed with a \'Y\'.'],
     'Y19000' => [],
     '190u' => ['Could not parse the date \'190u\'.'],
-    '190' => [],
-    '190-99-52' => [
+    '190' => ['Years must be at least 4 characters long.'],
+    '190-99-52' => ['Years must be at least 4 characters long.',
       'Provided month value \'99\' is not valid.',
       'Provided day value \'52\' is not valid.',
     ],
@@ -53,46 +53,8 @@ class EdtfUtilsTest extends KernelTestBase {
    * @covers ::validate
    */
   public function testEdtfValidate() {
-    foreach ($this->singleDateValidations as $input => $expected) {
+    foreach ($this->testCases as $input => $expected) {
       $this->assertEquals($expected, EDTFUtils::validate($input, FALSE, FALSE, FALSE));
-    }
-  }
-
-  /**
-   * @covers ::iso8601Value
-   */
-  public function testIso8601() {
-    // EDTF value and ISO 8601 Timestamp results.
-    // Empty values are invalid dates which return blank.
-    $tests = [
-      '1900' => '1900-01-01T00:00:00',
-      '1900-01' => '1900-01-01T00:00:00',
-      '1900-01-02' => '1900-01-02T00:00:00',
-      '190X' => '1900-01-01T00:00:00',
-      '1900-XX' => '1900-01-01T00:00:00',
-      '1900-91' => '',
-      '1900-91-01' => '',
-      '1900-3X' => '',
-      '1900-31' => '1900-03-01T00:00:00',
-      '190X-5X-8X' => '',
-      '19000' => '',
-      'Y19000' => '19000-01-01T00:00:00',
-      '190u' => '',
-      '190' => '190-01-01T00:00:00',
-      '190-99-52' => '',
-      '1900-01-02T' => '',
-      '1900-01-02T1:1:1' => '',
-      '1900-01-02T01:22:33' => '1900-01-02T01:22:33',
-      '1900-01-02T01:22:33Z' => '1900-01-02T01:22:33Z',
-      '1900-01-02T01:22:33+' => '',
-      '1900-01-02T01:22:33+05:00' => '1900-01-02T01:22:33+05:00',
-      // Intervals and Sets should return the earliest value.
-      '1900/2023' => '1900-01-01T00:00:00',
-      '[1900,2023]' => '1900-01-01T00:00:00',
-      '[1900,2023}' => '1900-01-01T00:00:00',
-    ];
-    foreach ($tests as $date => $iso) {
-      $this->assertEquals($iso, EDTFUtils::iso8601Value($date));
     }
   }
 
