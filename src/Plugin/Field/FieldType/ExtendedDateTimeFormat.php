3ccb0c2344aab7671c8b7d807ca2cdf026f7980c
Noel Chiasson <noel@discoverygarden.ca>
2025-06-02 12:57:49 -0300

    Addressing PHPCBF sniffs.

--- src/Plugin/Field/FieldType/ExtendedDateTimeFormat.php
+++ src/Plugin/Field/FieldType/ExtendedDateTimeFormat.php
@@ -26,7 +26,8 @@ class ExtendedDateTimeFormat extends StringItem {
    */
   public static function generateSampleValue(FieldDefinitionInterface $field_definition) {
     // Setting a valid basic EDTF year value.
-    $values = (string)mt_rand(1600, 9999);
+    $values = (string) mt_rand(1600, 9999);
     return $values;
   }
+
 }
